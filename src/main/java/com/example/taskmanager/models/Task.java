621f6b6f1329ff0422dd116fc0370eeae9669f13
Renata Casoni <renatacasoni6@gmail.com>
2025-06-17 13:12:22 -0300

    models

---
+++ src/main/java/com/example/taskmanager/models/Task.java
@@ -0,0 +1,24 @@
+package com.example.taskmanager.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+}
